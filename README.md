06b20024c87ea21cf227fba57025e60f903ab6e1
Oleg <chulkov@sibvaleo.com>
2022-11-09 01:35:07 +0700

    phpbook

--- README.md
+++ README.md
@@ -2,5 +2,12 @@
 
 vue ui -H 0.0.0.0
 
-8080 - для UI
-8000 - dev сервер
+8081 - для UI
+8001 - dev сервер
+
+# install
+
+    make build
+    make enter
+    npm i
+    npm run serve
--- blog/src/router.js
+++ blog/src/router.js
@@ -12,8 +12,8 @@ export default new Router({
       component: Main
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/author',
+      name: 'author',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
